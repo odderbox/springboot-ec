8a3e36bf5ddb17268c1deb45a1bbe3f377565008
odderbox <odderbox@gmail.com>
2022-04-08 22:46:32 +0800

    商品功能 - 改寫category，使用Enum類型

--- src/main/java/com/hhchen/springbootec/model/Product.java
+++ src/main/java/com/hhchen/springbootec/model/Product.java
@@ -7,7 +7,7 @@ import java.util.Date;
 public class Product {
     private Integer productId;
     private String productName;
-    private String category;
+    private ProductCategory category;
     private String imageUrl;
     private Integer price;
     private Integer stock;
@@ -31,11 +31,11 @@ public class Product {
         this.productName = productName;
     }
 
-    public String getCategory() {
+    public ProductCategory getCategory() {
         return category;
     }
 
-    public void setCategory(String category) {
+    public void setCategory(ProductCategory category) {
         this.category = category;
     }
 
@@ -87,8 +87,3 @@ public class Product {
         this.lastModifiedDate = lastModifiedDate;
     }
 }
-
-
-
-
-
--- src/main/java/com/hhchen/springbootec/rowmapper/ProductRowMapper.java
+++ src/main/java/com/hhchen/springbootec/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.hhchen.springbootec.rowmapper;
 
+import com.hhchen.springbootec.constant.ProductCategory;
 import com.hhchen.springbootec.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -13,7 +14,7 @@ public class ProductRowMapper implements RowMapper<Product> {
         Product product = new Product();
         product.setProductId(resultSet.getInt("product_id"));
         product.setProductName(resultSet.getString("product_name"));
-        product.setCategory(resultSet.getString("category"));
+        product.setCategory(ProductCategory.valueOf(resultSet.getString("category")) );
         product.setImageUrl(resultSet.getString("image_url"));
         product.setPrice(resultSet.getInt("price"));
         product.setStock(resultSet.getInt("stock"));
